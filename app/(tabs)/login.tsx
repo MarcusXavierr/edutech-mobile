460cae5a34c6ebc72930891b45c170f0116820cb
Marcus Xavier <marcusxavierr123@gmail.com>
2025-05-30 20:00:50 -0300

    first commit

---
+++ app/(tabs)/login.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>Algum filho da puta tem um pouco de crack?</Text>
+    </View>
+  );
+}
