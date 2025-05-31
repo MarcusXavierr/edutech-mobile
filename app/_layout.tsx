1c67507b0749a5f5d8b68293164e3fe83889de8b
Marcus Xavier <marcusxavierr123@gmail.com>
2025-05-31 09:49:32 -0300

    feat: implementando lógica para mostrar a tela de auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,35 @@
-import { Stack } from "expo-router";
+import { AuthProvider } from "@/plugins/auth-context";
+import { Stack, useRouter } from "expo-router";
+import React, { ReactNode, useEffect } from "react";
+import "../global.css";
+
+function RouteGuard({ children }: { children: ReactNode }) {
+  const router = useRouter();
+  const isAuth = false;
+
+  useEffect(() => {
+    // Small delay to ensure the navigator is mounted
+    // BUG: Fix this later
+    const timer = setTimeout(() => {
+      if (!isAuth) {
+        router.replace("/auth");
+      }
+    }, 100);
+
+    return () => clearTimeout(timer);
+  }, [isAuth, router]);
+
+  return <>{children}</>;
+}
 
 export default function RootLayout() {
   return (
-    <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }}/>
-      </Stack>
-    </>
-  )
+    <AuthProvider>
+      <RouteGuard>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </RouteGuard>
+    </AuthProvider>
+  );
 }
